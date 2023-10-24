a383c7dd7331893e64d908e8222d3c86f780263c
aliameur <aliameur169@gmail.com>
2023-10-24 16:11:18 +0100

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <ScrollableProjects>
         <ProjectVideo src="https://cdn.dribbble.com/userupload/3237703/file/original-6a7372febb8e52298529db2e32a0d157.mp4" />
         <ProjectVideo src="https://cdn.dribbble.com/userupload/3788399/file/original-6d3ba574e2f2c4d7df15f11f5581c29b.mp4" />
-        <ProjectImg src="/Project-3.jpeg" />
+        <ProjectImg src="/project-3.jpeg" />
         <ProjectVideo src="https://cdn.dribbble.com/users/1904192/screenshots/17820238/media/bff6c04dae85aa9cb1880d283d8780a6.mp4" />
         <ProjectVideo src="https://cdn.dribbble.com/users/11087757/screenshots/19727026/media/e9dc32a8ccc2b5470baf6eabf0422d41.mp4" />
       </ScrollableProjects>
